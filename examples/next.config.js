6c345f3368048c00b28ac5209374c83c1cbbf322
Bela Bohlender <bbohlender@coconut-capital.com>
2021-11-26 21:43:30 +0100

    fix css

--- examples/next.config.js
+++ examples/next.config.js
@@ -1,26 +1,19 @@
-const withPurgeCss = require("next-purgecss")
 const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/,
     options: {
         remarkPlugins: [require("remark-prism")],
     },
 })
-module.exports = withMDX(
-    withPurgeCss({
-        pageExtensions: ["tsx", "md", "mdx"],
-        images: {
-            loader: "custom",
-        },
-        basePath: "/co-flex",
-        assetPrefix: "/co-flex",
-        eslint: {
-            ignoreDuringBuilds: true,
-        },
-        pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-        trailingSlash: true,
-        purgeCssPaths: ["pages/**/*", "components/**/*"],
-        purgeCss: {
-            safelist: ["body", "html"],
-        },
-    })
-)
+module.exports = withMDX({
+    pageExtensions: ["tsx", "md", "mdx"],
+    images: {
+        loader: "custom",
+    },
+    basePath: "/co-flex",
+    assetPrefix: "/co-flex",
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    trailingSlash: true,
+})
