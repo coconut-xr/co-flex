171fb8af78d9f555c3ce3db376df02bb94e5b0a7
Bela Bohlender <bbohlender@coconut-capital.com>
2022-11-04 13:59:13 +0100

    fix property change warning / bug

--- src/use-bind-yoga-node-properties.ts
+++ src/use-bind-yoga-node-properties.ts
@@ -1,30 +1,56 @@
 import { FlexNode, YogaNodeProperties } from "co-yoga"
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useMemo } from "react"
 import { FlexNodeContext } from "."
+
+const emptyObjectFunction = () => ({})
 
 export function useBindFlexNodeProperties(
     node: FlexNode,
     requestLayoutCalculation: FlexNodeContext["requestLayoutCalculation"],
     properties: YogaNodeProperties
 ): void {
-    const prevProps = useRef<Partial<YogaNodeProperties>>({})
-    useEffect(() => {
-        Object.freeze(properties)
-        const prev: Partial<YogaNodeProperties> = prevProps.current
+    const prev = useMemo<{
+        node?: FlexNode
+        requestLayoutCalculation?: FlexNodeContext["requestLayoutCalculation"]
+        properties?: YogaNodeProperties
+    }>(emptyObjectFunction, [])
+
+    if (
+        prev.node === node &&
+        prev.properties === properties &&
+        prev.requestLayoutCalculation === requestLayoutCalculation
+    ) {
+        return
+    }
+
+    let changed = false
+
+    const prevProperties: YogaNodeProperties | undefined = prev.properties
+    const propertyEntries = Object.entries(properties)
+    if (prevProperties == null) {
+        changed = true
+        for (const [key, value] of propertyEntries) {
+            node.setProperty(key as any, value)
+        }
+    } else {
         for (const [key, value] of Object.entries(properties)) {
-            if (value != prev[key as keyof YogaNodeProperties]) {
+            if (value != prevProperties[key as keyof YogaNodeProperties]) {
                 node.setProperty(key as any, value)
+                changed = true
             }
-            delete prev[key as keyof YogaNodeProperties]
+            delete prevProperties[key as keyof YogaNodeProperties]
         }
-        for (const [key] of Object.entries(prev)) {
+        for (const [key] of Object.entries(prevProperties)) {
             node.setProperty(key as any, undefined)
+            changed = true
         }
-        prevProps.current = { ...properties }
+    }
+
+    prev.properties = properties
+    prev.node = node
+    prev.requestLayoutCalculation = requestLayoutCalculation
+
+    if (changed) {
         requestLayoutCalculation()
-    }, [
-        node,
-        requestLayoutCalculation,
-        ...Object.entries(properties).reduce<Array<any>>((v1, v2) => v1.concat(v2), []),
-    ])
+    }
 }
